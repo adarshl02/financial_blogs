14c8fdc85e6c2c1ab5d1f469f67ac8396c62b59e
adarsh landge <adarsh.landge10604@gmail.com>
2024-11-04 22:04:17 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from './pages/landing';
+import Analytics from './pages/Analytics';
+import Profile from './pages/Profile';
+import TipsAndSuggestions from './pages/TipsAndSuggestions';
+import Gullak from './pages/Gullak';
+import Diary from './pages/Diary';
+import Test from './pages/Test';
 
 export default function App() {
   return (
-    <div className='text-red-400' >App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/landing' element={<Landing/>}/>
+        <Route path='/analytics' element={<Analytics/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/tipsandsuggestions' element={<TipsAndSuggestions/>}/>
+        <Route path='/gullak' element={<Gullak/>}/>
+        <Route path='/diary' element={<Diary/>}/>
+        <Route path='/' element={<Test/>}/>
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/TipsAndSuggestions.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TipsAndSuggestions() {
+  return (
+    <div>TipsAndSuggestions</div>
+  )
+}
